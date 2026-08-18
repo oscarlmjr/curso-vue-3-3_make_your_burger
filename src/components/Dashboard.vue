9d368dae34fe2d081f12f96b36a14aea14d51ebb
oscarlmjr <oscarlmjr@gmail.com>
2026-08-18 08:34:50 -0300

    curso_30

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,6 +26,9 @@
         <div>
           <select name="status" class="status">
             <option value="">Selecione</option>
+            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+              {{ s.tipo }}
+            </option>
           </select>
           <button class="delete-btn">Cancelar</button>
         </div>
@@ -56,7 +59,16 @@ export default {
 
         console.log(this.burgers);
         // Resgata os status de pedidos
-        // this.getStatus();
+        this.getStatus();
+
+      },
+      async getStatus() {
+
+        const req = await fetch('http://localhost:3000/status');
+
+        const data = await req.json();
+
+        this.status = data;
 
       }
     },
