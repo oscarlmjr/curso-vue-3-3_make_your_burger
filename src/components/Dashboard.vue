019b14bd7dbe5b25d61888d4d182429d0d36a3c1
oscarlmjr <oscarlmjr@gmail.com>
2026-08-17 23:22:50 -0300

    curso_29

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,33 +11,16 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
-          </ul>
-        </div>
-        <div>
-          <select name="status" class="status">
-            <option value="">Selecione</option>
-          </select>
-          <button class="delete-btn">Cancelar</button>
-        </div>
-      </div>
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
-        <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+              {{ opcional }}
+            </li>
           </ul>
         </div>
         <div>
@@ -54,7 +37,32 @@
 <script>
 
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+    data() {
+      return {
+        burgers: null,
+        burger_id: null,
+        status: []
+      }
+    },
+    methods: {
+      async getPedidos() {
+
+        const req = await fetch('http://localhost:3000/burgers');
+
+        const data = await req.json();
+
+        this.burgers = data;
+
+        console.log(this.burgers);
+        // Resgata os status de pedidos
+        // this.getStatus();
+
+      }
+    },
+    mounted() {
+    this.getPedidos();
+    }
 }
 </script>
 
